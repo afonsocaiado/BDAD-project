d57f46fe2584beafa516b68d0918e16db823d15a
afonsocaiado <afonsocaiado1@gmail.com>
2020-04-01 16:50:26 +0100

    Mais create.sql

--- create.sql
+++ create.sql
@@ -6,10 +6,24 @@ PRAGMA foreign_keys = on;
 -- Table: PessoaFrequenteShopping
 DROP TABLE IF EXISTS PessoaFrequenteShopping;
 CREATE TABLE PessoaFrequenteShopping (
-  nif               INTEGER PRIMARY KEY,
+  nif               INTEGER PRIMARY KEY CHECK(LENGTH(nif == 9)),
   nome              STRING NOT NULL,
-  morada            STRING NOT NULL,
-  codigoPostal      INTEGER NOT NULL CHECK(...),
-  telefone          INTEGER NOT NULL CHECK(LENGTH(telefone == 9))         
+  morada            STRING,
+  codigoPostal      INTEGER,
+  telefone          INTEGER CHECK(LENGTH(telefone == 9))         
+);
+
+-- Table: Cliente
+DROP TABLE IF EXISTS Cliente;
+CREATE TABLE Cliente (
+  nif               INTEGER REFERENCES PessoaFrequenteShopping (nif) PRIMARY KEY CHECK(LENGTH(nif == 9)),
+  email             STRING NOT NULL                     
+);
+
+-- Table: Funccionario
+DROP TABLE IF EXISTS Funcionario;
+CREATE TABLE Funcionario (
+  nif               INTEGER REFERENCES PessoaFrequenteShopping (nif) PRIMARY KEY CHECK(LENGTH(nif == 9)),
+  salario           INTEGER CHECK(salario > 0)                    
 );
 
