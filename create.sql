6c02312b6d637e70c71e9586fa60cf5b3964ae63
afonsocaiado <afonsocaiado1@gmail.com>
2020-04-01 17:21:00 +0100

    Mais

--- create.sql
+++ create.sql
@@ -6,24 +6,84 @@ PRAGMA foreign_keys = on;
 -- Table: PessoaFrequenteShopping
 DROP TABLE IF EXISTS PessoaFrequenteShopping;
 CREATE TABLE PessoaFrequenteShopping (
-  nif               INTEGER PRIMARY KEY CHECK(LENGTH(nif == 9)),
-  nome              STRING NOT NULL,
-  morada            STRING,
-  codigoPostal      INTEGER,
-  telefone          INTEGER CHECK(LENGTH(telefone == 9))         
+  nif                   INTEGER PRIMARY KEY CHECK(LENGTH(nif == 9)),
+  nome                  STRING NOT NULL,
+  morada                STRING,
+  codigoPostal          INTEGER,
+  telefone              INTEGER CHECK(LENGTH(telefone == 9))         
 );
 
 -- Table: Cliente
 DROP TABLE IF EXISTS Cliente;
 CREATE TABLE Cliente (
-  nif               INTEGER REFERENCES PessoaFrequenteShopping (nif) PRIMARY KEY CHECK(LENGTH(nif == 9)),
-  email             STRING NOT NULL                     
+  nif                   INTEGER REFERENCES PessoaFrequenteShopping (nif) PRIMARY KEY CHECK(LENGTH(nif == 9)),
+  email                 STRING NOT NULL                     
 );
 
--- Table: Funccionario
+-- Table: Funcionario
 DROP TABLE IF EXISTS Funcionario;
 CREATE TABLE Funcionario (
-  nif               INTEGER REFERENCES PessoaFrequenteShopping (nif) PRIMARY KEY CHECK(LENGTH(nif == 9)),
-  salario           INTEGER CHECK(salario > 0)                    
+  nif                   INTEGER REFERENCES PessoaFrequenteShopping (nif) PRIMARY KEY CHECK(LENGTH(nif == 9)),
+  salario               INTEGER CHECK(salario > 0)                    
+);
+
+-- Table: FuncionarioEstabelecimento
+DROP TABLE IF EXISTS FuncionarioEstabelecimento;
+CREATE TABLE FuncionarioEstabelecimento (
+  nif                   INTEGER REFERENCES Funcionario (nif) PRIMARY KEY CHECK(LENGTH(nif == 9)),
+  nomeE                 STRING REFERENCES Estabelecimento (nomeE)                 
+);
+
+-- Table: FuncionarioShopping
+DROP TABLE IF EXISTS FuncionarioShopping;
+CREATE TABLE FuncionarioShopping (
+  nif                   INTEGER REFERENCES Funcionario (nif) PRIMARY KEY CHECK(LENGTH(nif == 9)),
+  profissao             STRING NOT NULL                
+);
+
+-- Table: Modelo
+DROP TABLE IF EXISTS Modelo;
+CREATE TABLE Modelo (
+  nomeModelo            STRING PRIMARY KEY NOT NULL,
+  nomeMarca             STRING REFERENCES Marca (nomeMarca) NOT NULL             
+);
+
+-- Table: Marca
+DROP TABLE IF EXISTS Marca;
+CREATE TABLE Marca (
+  nomeMarca             STRING PRIMARY KEY              
+);
+
+-- Table: Carro
+DROP TABLE IF EXISTS Carro;
+CREATE TABLE Carro (
+  matricula             INTEGER PRIMARY KEY,
+  nomeMarca             STRING REFERENCES Marca (nomeMarca),
+  nif                   INTEGER REFERENCES PessoaFrequenteShopping (nif) CHECK(LENGTH(nif == 9))     
+);
+
+-- Table: Produto
+DROP TABLE IF EXISTS Produto;
+CREATE TABLE Produto (
+  nomeP                 STRING NOT NULL,
+  codigo                INTEGER PRIMARY KEY,
+  custoUnitario         DOUBLE CHECK(custoUnitario >= 0),
+  quantidadeStock       INTEGER CHECK(quantidadeStock >= 0)   
+);
+
+-- Table: Devolucao
+DROP TABLE IF EXISTS Devolucao;
+CREATE TABLE Devolucao (
+  id                    INTEGER PRIMARY KEY,
+  data                  STRING NOT NULL, -- ou int nao sei  
+  nif                   INTEGER REFERENCES Cliente (nif) CHECK(LENGTH(nif == 9))                   
+);
+
+-- Table: ProdutoDevolvido
+DROP TABLE IF EXISTS ProdutoDevolvido;
+CREATE TABLE ProdutoDevolvido (
+  codigo                INTEGER REFERENCES Produto (codigo) PRIMARY KEY,
+  id                    INTEGER REFERENCES Devolucao (id),
+  quantidade            INTEGER CHECK (quantidade >= 0)               
 );
 
