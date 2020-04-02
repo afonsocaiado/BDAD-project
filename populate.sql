600bce0ccc843e062a09ccfb38c4fe2246056838
afonsocaiado <afonsocaiado1@gmail.com>
2020-04-02 13:54:24 +0100

    populate.sql inicio

---
+++ populate.sql
@@ -0,0 +1,12 @@
+
+-- Data : PessoaFrequenteShopping
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (111111111, 'João Castro', 'Rua Corte Real', 4150888, 9111111111);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (222222222, 'Tiago Delfim', 'Rua Gilberto Arenas', 4150888, 912222222);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (333333333, 'Manuel de Oliveira', 'Rua da Esquerda', 4150777, 913333333);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (444444444, 'Vitor Hugo', 'Avenida da Liberdade', 4150666, 914444444);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (555555555, 'Vasco Ferreira', 'Rua do Crasto', 4150555, 910000000);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (666666666, 'Carolina Mengo', 'Constituição', 4150444, 915555555);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (777777777, 'Miguel Afonso', 'Avenida da Boavista', 4150122, 916666666);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (888888888, 'Tomas Freitas', 'Rua do fundo', 4222122, 917777777);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (999999999, 'Rui Teixeira', 'Rua da Bandeirinha', 4150333, 918888888);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (101010101, 'Rony Lopes', 'Rua grande', 4150232, 919999999);
