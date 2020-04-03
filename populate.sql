ed45fc7ffbd0bc9e731bfbddd71ac3575b6aa5ba
afonsocaiado <afonsocaiado1@gmail.com>
2020-04-03 01:23:49 +0100

    Mais populate.sql

--- populate.sql
+++ populate.sql
@@ -8,5 +8,18 @@ INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) 
 INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (666666666, 'Carolina Mengo', 'Constituição', 4150444, 915555555);
 INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (777777777, 'Miguel Afonso', 'Avenida da Boavista', 4150122, 916666666);
 INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (888888888, 'Tomas Freitas', 'Rua do fundo', 4222122, 917777777);
-INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (999999999, 'Rui Teixeira', 'Rua da Bandeirinha', 4150333, 918888888);
+INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (999999999, 'Rui Teixeira', 'Rua da Bandeirinha', 4150333, 91888888);
 INSERT INTO PessoaFrequenteShopping (nif, nome, morada, codigoPostal, telefone) VALUES (101010101, 'Rony Lopes', 'Rua grande', 4150232, 919999999);
+
+-- Data : Cliente
+INSERT INTO Cliente (nif, email) VALUES (111111111, 'jcastro@gmail.com');
+INSERT INTO Cliente (nif, email) VALUES (222222222, 'tdelfim@gmail.com');
+INSERT INTO Cliente (nif, email) VALUES (333333333, 'moliveira@gmail.com');
+INSERT INTO Cliente (nif, email) VALUES (444444444, 'vhugo@gmail.com');
+INSERT INTO Cliente (nif, email) VALUES (555555555, 'vferreira@gmail.com');
+INSERT INTO Cliente (nif, email) VALUES (666666666, 'cmengo@gmail.com');
+INSERT INTO Cliente (nif, email) VALUES (777777777, 'mafonso@gmail.com');
+INSERT INTO Cliente (nif, email) VALUES (888888888, 'tfreitas@gmail.com');
+INSERT INTO Cliente (nif, email) VALUES (999999999, 'rteixeira@gmail.com');
+INSERT INTO Cliente (nif, email) VALUES (101010101, 'rlopes@gmail.com');
+
