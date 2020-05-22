0961777909d05473829a7942431718738ef00ba3
Fabio Moreira <moreira.fbio8@gmail.com>
2020-05-22 13:39:44 +0100

    Adjusted second query

--- interrogacoes/int2.sql
+++ interrogacoes/int2.sql
@@ -4,9 +4,10 @@
 .nullvalue NULL
 
 --Qual foi o cliente com o maior valor total de compras?
-Select nome, SUM(montante) as total 
-FROM (PessoaFrequenteShopping , Compra) 
-WHERE (Compra.nif = PessoaFrequenteShopping.nif) 
-GROUP BY PessoaFrequenteShopping.nif 
-ORDER BY total DESC 
-limit 1;
+Select nome, MAX(total) as Total FROM 
+
+    (Select nome, SUM(montante) as total 
+    FROM (PessoaFrequenteShopping , Compra) 
+    WHERE (Compra.nif = PessoaFrequenteShopping.nif) 
+    GROUP BY PessoaFrequenteShopping.nif )
+
