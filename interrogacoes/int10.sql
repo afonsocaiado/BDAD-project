e4059fb2cffbeb3e2577093f135026d8e23cd528
Fabio Moreira <moreira.fbio8@gmail.com>
2020-05-22 17:12:24 +0100

    Added 10th query

---
+++ interrogacoes/int10.sql
@@ -0,0 +1,9 @@
+.mode columns 
+.headers ON
+.nullvalue NULL
+
+--Quantas compras fez cada cliente, assim como o montante total
+SELECT nome, COUNT(*) as Compras, SUM(montante) as Total
+ FROM Compra NATURAL JOIN Cliente NATURAL JOIN PessoaFrequenteShopping 
+ GROUP BY nif
+ ORDER BY Compras;
